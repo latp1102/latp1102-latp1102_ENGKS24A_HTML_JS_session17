4fd014cdc351f4df2db261ca3eb3095eb201d5c5
latp1102 <latp1102@gmail.com>
2025-02-27 16:30:34 +0700

    Session17-6

---
+++ b6.js
@@ -0,0 +1,7 @@
+document.write(Number("5") + "<br>");
+document.write(Number("-1") + "<br>");
+document.write(Number("Hello") + "<br>");
+
+document.write(String("55") + "<br>");
+document.write(String("-10") + "<br>");
+document.write(String("0") + "<br>");
