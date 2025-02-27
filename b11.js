853f3e7a5842a6f58e416c46d13d3e8343066900
latp1102 <latp1102@gmail.com>
2025-02-27 16:33:44 +0700

    Session17-11

---
+++ b11.js
@@ -0,0 +1,19 @@
+let number1 = Math.abs(-5.5);
+let number2 = Math.abs(10);
+document.write("<p> "+ number1+ "</p>");// Giá trị tuyệt đối 
+document.write("<p> "+ number2+ "</p>");// Giá trị tuyệt đối
+
+let number3 = Math.ceil(4.4);
+let number4 = Math.ceil(5.8);
+document.write("<p> "+ number3 +"</p>");// Làm tròn số nguyên gần đây nhất
+document.write("<p> "+ number4 +"</p>");// Làm tròn số nguyên gần đây nhất
+
+let number5 = Math.floor(5.7);// Làm tròn xuống số nguyên gần nhất
+let number6 = Math.floor(6.1);// Làm tròn xuống số nguyên gần nhất
+document.write("<p> "+ number5 +"</p>");
+document.write("<p> "+ number6 +"</p>");
+
+let number7 = Math.round(10.1);// Làm tròn đến số nguyên gần nhất
+let number8 = Math.round(20.6);// Làm tròn đến số nguyên gần nhất
+document.write("<p> "+ number7 +"</p>");
+document.write("<p> "+ number8 +"</p>");
