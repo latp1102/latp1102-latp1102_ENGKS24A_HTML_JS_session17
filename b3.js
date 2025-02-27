8eaf2ed5e5ad1fc107f84668f48d3946614b2339
latp1102 <latp1102@gmail.com>
2025-02-27 16:27:30 +0700

    Session17-3

---
+++ b3.js
@@ -0,0 +1,7 @@
+let angry = prompt("Mời bạn nhập họ và tên");
+angry.toUpperCase();
+document.write("<p> " + angry.toUpperCase() + "</p>");
+
+let angry_01 = prompt("Mời bạn nhập họ và tên");
+angry_01.toUpperCase();
+document.write("<p> " + angry_01.toUpperCase() + "</p>");
